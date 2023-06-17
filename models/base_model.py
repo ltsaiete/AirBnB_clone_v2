08a82704f24bef5339adabeb90dffa007ae4b97c
Luis Saiete <luistsaiete@gmail.com>
2023-06-17 17:17:32 +0200

    Create models with kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -2,25 +2,37 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
         else:
+            if kwargs.get('updated_at') is None:
+                kwargs['updated_at'] = datetime.now().isoformat()
+            if kwargs.get('created_at') is None:
+                kwargs['created_at'] = datetime.now().isoformat()
+            if kwargs.get('id') is None:
+                kwargs['id'] = str(uuid.uuid4())
+
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+
+            if kwargs.get('__class__') is not None:
+                del kwargs['__class__']
+            # self.__dict__.update(kwargs)
+            for k, v in kwargs.items():
+                setattr(self, k, v)
 
     def __str__(self):
         """Returns a string representation of the instance"""
