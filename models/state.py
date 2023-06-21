472cd353251077d35d2fc8ee77ee6b86e0410cd5
Luis Saiete <luistsaiete@gmail.com>
2023-06-21 23:42:28 +0200

    Define City and State models

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import os
+from models.base_model import BaseModel, Base
+from models import storage
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+
+    __tablename__ = 'states'
+
+    name = Column(String(60), nullable=False)
+    cities = relationship('City', backref='state')
+
+    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            cities_list = []
+            cities_objs = storage.all(City)
+            for k, v in cities_objs.values():
+                if v.state_id == self.id:
+                    cities_list.append(v)
+
+            return cities_list
