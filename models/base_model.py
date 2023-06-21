4294907ec906c4caa66bd593cf84fafe7891c3af
Luis Saiete <luistsaiete@gmail.com>
2023-06-21 23:07:27 +0200

    Update BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -3,10 +3,18 @@
 import uuid
 from datetime import datetime
 from models import storage
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), primary_key=True)
+    created_at = Column(Datetime(), nullable=False, default=datetime.utcnow())
+    updated_at = Column(Datetime(), nullable=False,
+                        default=datetime.utcnow(), onupdate=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
@@ -14,13 +22,12 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             if kwargs.get('updated_at') is None:
                 kwargs['updated_at'] = datetime.now().isoformat()
             if kwargs.get('created_at') is None:
                 kwargs['created_at'] = datetime.now().isoformat()
-                
+
             if kwargs.get('id') is None:
                 kwargs['id'] = str(uuid.uuid4())
 
@@ -44,7 +51,11 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
+
+    def delete(self):
+        storage.delete(self)
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -54,4 +65,8 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+
+        if dictionary.get('_sa_instance_state') is not None:
+            del dictionary['_sa_instance_state']
+
         return dictionary
