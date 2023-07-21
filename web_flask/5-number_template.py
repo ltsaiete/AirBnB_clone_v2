6a55532ed7970b50933fb0564a1328e7bc400edd
Luis Saiete <luistsaiete@gmail.com>
2023-07-21 23:10:26 +0200

    Add tasks 5 and 6

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+This is a simple module that starts a Flask web application
+listening on 0.0.0.0, port 5000
+"""
+from flask import Flask, render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+app.config['RESTFUL_URL_PREFIX'] = False
+
+
+@app.route('/')
+def index():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    return f'C {escape(text).replace("_", " ")}'
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def py_text(text='is cool'):
+    return f'Python {escape(text).replace("_", " ")}'
+
+
+@app.route('/number/<int:n>')
+def show_number(n):
+    return f'{escape(n)} is a number'
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
