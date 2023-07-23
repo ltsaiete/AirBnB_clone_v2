39760e0ae0ef44bcbbd876824351484240aed22a
Luis Saiete <luistsaiete@gmail.com>
2023-07-23 14:51:04 +0200

    Remove dotenv module

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from os import getenv
-from dotenv import load_dotenv
-
-load_dotenv()
 
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
